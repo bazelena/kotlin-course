72f96efc4e841e911ce45c99edf0378647292799
Helena <bazelenamix@gmail.com>
2026-09-25 22:34:39 +0400

    commit

--- src/main/kotlin/com/lessons/lesson07/homeworks/Homeworks.kt
+++ src/main/kotlin/com/lessons/lesson07/homeworks/Homeworks.kt
@@ -2,7 +2,7 @@ package com.lessons.lesson07.homeworks
 
 // Задания для цикла for
 
-fun main () {
+fun main() {
 
     val collection = listOf<String>("1", "2")
     for (item in collection) {
@@ -21,43 +21,40 @@ fun main () {
         if (i % 2 == 0) {
             println(i)
         }
+    }
 
 // Обратный диапазон
 
 // 3. Создайте цикл for, который выводит числа от 5 до 1.
-        for (i in 5 downTo 1)
-            println(i)
+    for (i in 5 downTo 1)
+        println(i)
 
 // 4. Создайте цикл for, который выводит числа от 10 до 1, уменьшая их на 2.
-        for (i in 10 downTo 1) { // уменьшение числа на 2
-            println(i - 2)
-        }
-
-        for (i in 10 downTo 1 step 2) { // уменьшение числа при каждом следующем шаге на 2
-            println(i)
-        }
+    for (i in 10 downTo 1 step 2) { // уменьшение числа при каждом следующем шаге на 2
+        println(i)
+    }
 
 // С шагом (step)
 
 // 5. Используйте цикл for с шагом 2 для вывода чисел от 1 до 9.
-        for (i in 1..9 step 2) {
-            println(i)
-        }
+    for (i in 1..9 step 2) {
+        println(i)
+    }
 
 // 6. Напишите цикл for, который выводит каждое третье число в диапазоне от 1 до 20.
-        for (i in 1 ..20 step 3) {
-            println(i)
-        }
+    for (i in 1..20 step 3) {
+        println(i)
+    }
 
 // Использование до (until)
 
 // 7. Создайте числовую переменную 'size'. Используйте цикл for с шагом 2
 // для вывода чисел от 3 до size не включая size.
-        val size = 10
+    val size = 10
 
-        for (i in 3 until size step 2) {
-            println(i)
-        }
+    for (i in 3 until size step 2) {
+        println(i)
+    }
 
 
 // Задания для цикла while
@@ -65,70 +62,67 @@ fun main () {
 // Цикл while
 
 // 8. Создайте цикл while, который выводит квадраты чисел от 1 до 5.
-        var a = 1
-        while (a <= 5) {
-            println(a * a)
-            a++
-        }
+    var a = 1
+    while (a <= 5) {
+        println(a * a)
+        a++
+    }
 
 // 9. Напишите цикл while, который уменьшает число от 10 до 5. После этого вывести результат в консоль
-        var b = 10
-        while (b >= 5) {
-            println(b)
-            --b
-        }
+    var b = 10
+    while (b > 5) {
+        b--
+    }
+    println(b)
 
 // Цикл do while
 // 10. Используйте цикл do while, чтобы вывести числа от 5 до 1.
-        var e = 5
-        do {
-            println(e)
-        } while (--e > 1)
+    var e = 5
+    do {
+        println(e)
+    } while (e-- > 1)
 
 // 11. Создайте цикл do while, который повторяется, пока счетчик меньше 10, начиная с 5.
-        var counter = 5
+    var counter = 5
 
-        do {
-            println(counter)
-            counter++
-        } while (counter <10)
+    do {
+        println(counter)
+        counter++
+    } while (counter < 10)
 
 // Задания для прерывания и пропуска итерации
 
 // Использование break
 
 // 12. Напишите цикл for от 1 до 10 и используйте break, чтобы выйти из цикла при достижении 6.
-        for (i in 1..10) {
-            if (i == 6) break
-            println(i)
-        }
+    for (i in 1..10) {
+        if (i == 6) break
+        println(i)
+    }
 
 // 13. Создайте цикл while, который бесконечно выводит числа, начиная с 1, но прерывается при достижении 10.
-        var c = 1
-        while (true) {
-            println(c)
-            if (c == 10) {
-                break
-            }
-            c++
+    var c = 1
+    while (true) {
+        println(c)
+        if (c == 10) {
+            break
         }
+        c++
+    }
 
 // Использование continue
 
 // 14. В цикле for от 1 до 10 используйте continue, чтобы пропустить четные числа.
-        for (i in 1..10) {
-            if (i % 2 == 0) continue
-            println(i)
-        }
+    for (i in 1..10) {
+        if (i % 2 == 0) continue
+        println(i)
+    }
 
 // 15. Напишите цикл while, который выводит числа от 1 до 10, но пропускает числа, кратные 3.
-        var d = 1
-        while (d <= 10) {
-            if (d % 3 == 0) {
-                d++
-                continue
-            }
+    var d = 1
+    while (d <= 10) {
+        if (d % 3 != 0) {
             println(d)
-            d++
         }
-
+        d++
+    }
